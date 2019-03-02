b6dd6d387cae4a32850e57d5caa1e88b61fa3ad4
Mike Ferchak <mikeferchak@gmail.com>
2019-03-02 00:24:41 -0500

    first pass on watch app

--- PAXWatch Extension/InterfaceController.swift
+++ PAXWatch Extension/InterfaceController.swift
@@ -11,14 +11,28 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet weak var thatTimeLabel: WKInterfaceLabel!
+    @IBOutlet weak var thisClassLabel: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
+        let defaults = Defaults()
+        
+//        let thisPaxIndex = Scca().pax_at_index(defaults.thisPaxPicker)
+//        let thatPaxIndex = Scca().pax_at_index(defaults.thatPaxPicker)
+//        
+//        
+//        let thatTime:Double = Calculator(this_time: defaults.thisTime, this_pax: thisPaxIndex, that_pax: thatPaxIndex).result
+//        
+//        let thatTimeString = String(thatTime)
+//        let thatClassString = Scca().class_name_at_index(defaults.thatPaxPicker)
+//        
+//        thatTimeLabel.setText(thatTimeString)
+//        thisClassLabel.setText(thatClassString)
+        
         super.willActivate()
     }
     
--- PAXWatch Extension/PickerController.swift
+++ PAXWatch Extension/PickerController.swift
@@ -15,6 +15,20 @@ class PickerController: WKInterfaceController {
     @IBOutlet var hundrethsPicker: WKInterfacePicker!
     @IBOutlet var thousandthsPicker: WKInterfacePicker!
     
+    @IBOutlet var thisClassPicker: WKInterfacePicker!
+    @IBOutlet var thatClassPicker: WKInterfacePicker!
+    
+    @IBOutlet weak var thatTimeLabel: WKInterfaceLabel!
+    @IBOutlet weak var thatClassLabel: WKInterfaceLabel!
+    
+    var thisClassPickerValue = 0
+    var thatClassPickerValue = 0
+    var thisTime = 0.00
+    var thisSeconds:Int = 0
+    var thisTenths:Int = 0
+    var thisHundreths:Int = 0
+    var thisThousanths:Int = 0
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -22,12 +36,49 @@ class PickerController: WKInterfaceController {
     }
     
     override func willActivate() {
+        //var defaults = Defaults.init()
+        
         setupSecondsPicker()
         setupTenthsPicker()
         setupHundrethsPicker()
         setupThousandthsPicker()
         
+        setupThisClassPicker()
+        setupThatClassPicker()
+        
+        applyDefaults()
+        
         super.willActivate()
+    }
+    
+    func applyDefaults() {
+        let defaults = Defaults()
+        
+        thisClassPicker.setSelectedItemIndex(defaults.thisPaxPicker);
+        thatClassPicker.setSelectedItemIndex(defaults.thatPaxPicker);
+        
+        let time = defaults.thisTime
+        
+        let seconds = modf(time).0
+        
+        let decimals = String(modf(time).1 * 1000)
+        let decimalsTokenized = decimals.compactMap({Int(String($0))})
+        
+        if decimalsTokenized.count == 3 {
+            let tenths = decimalsTokenized[0]
+            let hundreths = decimalsTokenized[1]
+            let thousandths = decimalsTokenized[2]
+            
+            secondsPicker.setSelectedItemIndex(Int(seconds))
+            tenthsPicker.setSelectedItemIndex(tenths)
+            hundrethsPicker.setSelectedItemIndex(hundreths)
+            thousandthsPicker.setSelectedItemIndex(thousandths)
+        }
+        
+        
+     
+        
+        updateLabels()
     }
     
     override func didDeactivate() {
@@ -83,17 +134,96 @@ class PickerController: WKInterfaceController {
         thousandthsPicker.setItems(pickerItems)
     }
     
+    func setupThisClassPicker() {
+        var pickerItems:[WKPickerItem] = []
+        
+        let classNames:[String] = Scca().classNames;
+        
+        for className in classNames {
+            let pickerItem = WKPickerItem()
+            pickerItem.title = className
+            pickerItem.caption = className
+            pickerItems += [pickerItem]
+        }
+        
+        thisClassPicker.setItems(pickerItems)
+    }
     
+    func setupThatClassPicker() {
+        var pickerItems:[WKPickerItem] = []
+
+        let classNames:[String] = Scca().classNames;
+
+        for className in classNames {
+            let pickerItem = WKPickerItem()
+            pickerItem.title = className
+            pickerItem.caption = className
+            pickerItems += [pickerItem]
+        }
+
+        thatClassPicker.setItems(pickerItems)
+    }
+    
+    func calculateTime() -> Double {
+        var result:Double = 0.0
+        result += Double(thisSeconds)
+        result += Double(thisTenths) / 10.0
+        result += Double(thisHundreths) / 100.0
+        result += Double(thisThousanths) / 1000.0
+        
+        return result
+    }
+    
+    func updateLabels() {
+        let thisPaxIndex = Scca().pax_at_index(thisClassPickerValue)
+        let thatPaxIndex = Scca().pax_at_index(thatClassPickerValue)
+        
+        let thatTime:Double = Calculator(this_time: thisTime, this_pax: thisPaxIndex, that_pax: thatPaxIndex).result
+        
+        let thatTimeString = String(thatTime)
+        let thatClassString = Scca().class_name_at_index(thatClassPickerValue)
+        
+        thatTimeLabel.setText(thatTimeString)
+        thatClassLabel.setText(thatClassString)
+    }
     
     @IBAction func secondsPickerChanged(_ value: Int) {
+        thisSeconds = value
+        thisTime = calculateTime()
+        updateLabels()
+        Defaults().set(thisTime, thisPaxPickerNew: thisClassPickerValue, thatPaxPickerNew: thatClassPickerValue)
     }
 
     @IBAction func tenthsPickerChanged(_ value: Int) {
+        thisTenths = value
+        thisTime = calculateTime()
+        updateLabels()
+        Defaults().set(thisTime, thisPaxPickerNew: thisClassPickerValue, thatPaxPickerNew: thatClassPickerValue)
     }
     
     @IBAction func hundrethsPickerChanged(_ value: Int) {
+        thisHundreths = value
+        thisTime = calculateTime()
+        updateLabels()
+        Defaults().set(thisTime, thisPaxPickerNew: thisClassPickerValue, thatPaxPickerNew: thatClassPickerValue)
     }
     
     @IBAction func thousandthsPickerChanged(_ value: Int) {
+        thisThousanths = value
+        thisTime = calculateTime()
+        updateLabels()
+        Defaults().set(thisTime, thisPaxPickerNew: thisClassPickerValue, thatPaxPickerNew: thatClassPickerValue)
+    }
+    
+    @IBAction func thisClassPickerChanged(_ value: Int) {
+        thisClassPickerValue = value
+        updateLabels()
+        Defaults().set(thisTime, thisPaxPickerNew: thisClassPickerValue, thatPaxPickerNew: thatClassPickerValue)
+    }
+    
+    @IBAction func thatClassPickerChanged(_ value: Int) {
+        thatClassPickerValue = value
+        updateLabels()
+        Defaults().set(thisTime, thisPaxPickerNew: thisClassPickerValue, thatPaxPickerNew: thatClassPickerValue)
     }
 }
